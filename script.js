e0773f3ac0b91923a39d63475bf439b5deb0b3a8
tengkusripratiwi <suksesjaya0802@gmail.com>
2024-09-18 01:08:12 -0700

    Menentukan bilangan ganjil dan genap dengan js

---
+++ script.js
@@ -0,0 +1,8 @@
+var angka = prompt("Masukan Angka")
+if(angka % 2 === 0){
+    alert("Angka " + angka + " bilangan genap")
+}else if( angka % 2 === 1){
+    alert("Angka " + angka + " bilangan ganjil")
+}else{
+    alert( angka + " adalah huruf")
+}
